8b5b9b6e74a1393f30ef02284f34c92d382aee6d
Phi Hùng <112360424+phihung2004@users.noreply.github.com>
2026-08-13 10:46:43 +0700

    fix(L6): Change Prerender to Server

--- src/todo-app/src/app/app.routes.server.ts
+++ src/todo-app/src/app/app.routes.server.ts
@@ -3,6 +3,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    // renderMode: RenderMode.Prerender
+
+    // [FIX L6]: Đổi sang Server (SSR) để xử lý các route động có tham số như :filter,
+    // tránh lỗi crash khi chạy lệnh `ng build`
+    renderMode: RenderMode.Server,
+  },
 ];
